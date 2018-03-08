ecaf0142511c0a17e834fb5217f2bd161b75e86c
Peter Kremsner <peterkremsner@gmx.at>
2018-03-08 12:58:15 +0100

    Adds __HAL_TIM_GET_DUTYCYCLE

--- Inc/utils.h
+++ Inc/utils.h
@@ -11,5 +11,6 @@
 #include "stm32f4xx_hal.h"
 
 #define __HAL_TIM_SET_DUTYCYCLE(htim, channel, dutycycle) (__HAL_TIM_SET_COMPARE(htim, channel, __HAL_TIM_GET_AUTORELOAD(htim) * (dutycycle > 100 ? 100 : dutycycle)/100))
+#define __HAL_TIM_GET_DUTYCYCLE(htim, channel) (__HAL_TIM_GET_COMPARE(htim, channel) / (__HAL_TIM_GET_AUTORELOAD(htim) * 100))
 
 #endif /* UTILS_H_ */
